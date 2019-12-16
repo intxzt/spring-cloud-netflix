778535694b6d5ac57dcc8f704596a88e2d8c9d29
intxzt <intxzt@163.com>
2019-12-16 17:47:47 +0800

    client

---
+++ cloud-admin-client/src/test/java/com/xzt/admin/CloudAdminClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzt.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAdminClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
