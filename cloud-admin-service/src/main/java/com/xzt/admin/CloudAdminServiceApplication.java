cac941b34258661102243051cb78255985236b63
intxzt <intxzt@163.com>
2019-12-17 15:40:04 +0800

    admin service init

---
+++ cloud-admin-service/src/main/java/com/xzt/admin/CloudAdminServiceApplication.java
@@ -0,0 +1,15 @@
+package com.xzt.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class CloudAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudAdminServiceApplication.class, args);
+    }
+
+}
