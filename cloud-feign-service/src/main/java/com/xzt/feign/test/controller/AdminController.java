26fab55ed38b1b5ec972285d344ec31c57550bd3
xuzhengtong <intxzt@163.com>
2019-12-23 10:00:06 +0800

    feign init

--- cloud-feign-service/src/main/java/com/xzt/feign/test/controller/AdminController.java
+++ cloud-feign-service/src/main/java/com/xzt/feign/test/controller/AdminController.java
@@ -1,5 +1,6 @@
 package com.xzt.feign.test.controller;
 
+import com.xzt.feign.test.beans.User;
 import com.xzt.feign.test.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,5 +16,14 @@ public class AdminController {
     @RequestMapping(value = "hi", method = RequestMethod.GET)
     public String sayHi(@RequestParam String message) {
         return adminService.sayHi(message);
+    }
+
+    @RequestMapping(value = "req_post", method = RequestMethod.GET)
+    public String postHi(String userName, String password) {
+        User user = new User();
+        user.setUserName(userName);
+        user.setPassword(password);
+        User res = adminService.postHi(user);
+        return String.format("%s ,welcome! I am from port %s", userName, res.getPort());
     }
 }
--- cloud-feign-service/src/main/java/com/xzt/feign/test/service/AdminService.java
+++ cloud-feign-service/src/main/java/com/xzt/feign/test/service/AdminService.java
@@ -1,7 +1,9 @@
 package com.xzt.feign.test.service;
 
 
+import com.xzt.feign.test.beans.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,5 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(value = "cloud-admin-service")
 public interface AdminService {
     @RequestMapping(value = "hi", method = RequestMethod.GET)
-    public String sayHi(@RequestParam(value = "message") String message);
+    String sayHi(@RequestParam(value = "message") String message);
+
+    @RequestMapping(value = "req_post", method = RequestMethod.POST)
+    User postHi(@RequestBody User user);
 }
