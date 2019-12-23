09c3927bf62627c33d21f043a6ab8ec93a113bd4
xuzhengtong <intxzt@163.com>
2019-12-23 10:00:50 +0800

    feign init

---
+++ cloud-feign-service/src/main/java/com/xzt/feign/test/beans/User.java
@@ -0,0 +1,36 @@
+package com.xzt.feign.test.beans;
+
+public class User {
+    private String userName;
+    private String password;
+    private String port;
+
+
+    public User() {
+        super();
+    }
+
+    public String getPort() {
+        return port;
+    }
+
+    public void setPort(String port) {
+        this.port = port;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
