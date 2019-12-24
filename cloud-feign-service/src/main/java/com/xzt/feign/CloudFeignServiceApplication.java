0825a86f41a608428b422f794004e6c6f9095745
xuzhengtong <intxzt@163.com>
2019-12-24 14:52:44 +0800

    feign fuse dashboard

--- cloud-feign-service/src/main/java/com/xzt/feign/CloudFeignServiceApplication.java
+++ cloud-feign-service/src/main/java/com/xzt/feign/CloudFeignServiceApplication.java
@@ -3,11 +3,13 @@ package com.xzt.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class CloudFeignServiceApplication {
 
     public static void main(String[] args) {
